c537ae36e317b68de140a13d48d6b74db9f7542e
Priyanshu354 <titpriyanshuraj@gmail.com>
2024-04-25 12:10:13 +0530

    Project 3 Complete

---
+++ 03 - CSS Variables/script.js
@@ -0,0 +1,12 @@
+const inputs = document.querySelectorAll('.controls input');
+
+function handleChanges() {
+  const suff = this.dataset.sizing || '';
+  console.log(suff);
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + suff);
+  console.log(this.value);
+}
+
+inputs.forEach(input => input.addEventListener('change', handleChanges));
+// inputs.forEach(input => input.addEventListener('mousemove', handleChanges));
+
